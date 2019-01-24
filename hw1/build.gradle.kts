298f080aa1382f8aa21363db5dbecc8f99baaac7
Arkady Bazhanov <arkady.bazhanov@gmail.com>
2019-01-24 14:26:34 +0300

    Init hw1 project structure

---
+++ hw1/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    java
+}
+
+group = "me.arkadybazhanov.au"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile("org.jetbrains:annotations:16.0.2")
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    testCompile("org.junit.jupiter:junit-jupiter-params:5.3.2")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.2")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
