d407081d42d68cda878677d9324ca829d0fc530c
Arkady Bazhanov <arkady.bazhanov@gmail.com>
2019-02-06 16:47:26 +0300

    Fix prev commit

--- test/src/main/kotlin/me/arkadybazhanov/au/java/hw2/SmartList.kt
+++ test/src/main/kotlin/me/arkadybazhanov/au/java/hw2/SmartList.kt
@@ -1,4 +1,118 @@
 package me.arkadybazhanov.au.java.hw2
 
-class SmartList {
+import java.lang.IndexOutOfBoundsException
+
+/**
+ * An implementation of [MutableList] that is optimized for small number of elements
+ * If it contains single element, the reference to it is stored directly.
+ * If it contains from 2 to [SmartList.MAX_ARRAY_SIZE] elements, they are stored in an array.
+ * Else elements are stored in default kotlin mutable list.
+ */
+class SmartList<T>() : AbstractMutableList<T>() {
+
+    override var size: Int = 0
+        private set
+
+    private var elements: Any? = null
+
+    constructor(collection: Collection<T>) : this() {
+        size = collection.size
+        when (size) {
+            0 -> {}
+            1 -> elements = collection.single()
+            in 2..MAX_ARRAY_SIZE -> elements = collection.toTypedArray<Any?>().copyOf(MAX_ARRAY_SIZE)
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private val single: T get() = elements as T
+
+    @Suppress("UNCHECKED_CAST")
+    private val array: Array<T> get() = elements as Array<T>
+
+    @Suppress("UNCHECKED_CAST")
+    private val list: MutableList<T> get() = elements as MutableList<T>
+
+    override fun add(index: Int, element: T) {
+        boundsCheck(index, size + 1)
+        size++
+
+        if (size == 1) {
+            elements = element
+            return
+        }
+
+        if (size == 2) {
+            elements = arrayOf<Any?>(single, null, null, null, null)
+        }
+        if (size in 2..MAX_ARRAY_SIZE) {
+            for (i in size - 2 downTo index) {
+                array[i + 1] = array[i]
+            }
+            array[index] = element
+            return
+        }
+
+        if (size == MAX_ARRAY_SIZE + 1) {
+            elements = array.toMutableList()
+        }
+        list.add(index, element)
+    }
+
+    override fun get(index: Int): T {
+        boundsCheck(index)
+        return when (size) {
+            1 -> single
+            in 2..MAX_ARRAY_SIZE -> array[index]
+            else -> list[index]
+        }
+    }
+
+    private fun boundsCheck(index: Int, size: Int = this.size) {
+        if (index !in 0 until size) throw IndexOutOfBoundsException()
+    }
+
+    override fun removeAt(index: Int): T {
+        boundsCheck(index)
+        size--
+
+        if (size == 0) {
+            return single.also { elements = null }
+        }
+
+        if (size == 1) {
+            return array[index].also {
+                val otherIndex = 1 - index
+                elements = array[otherIndex]
+            }
+        }
+
+        if (size >= MAX_ARRAY_SIZE) {
+            val res = list.removeAt(index)
+            if (size == MAX_ARRAY_SIZE) {
+                elements = list.toTypedArray<Any?>()
+            }
+            return res
+        }
+
+        val res = array[index]
+        for (i in size - 1 downTo index) {
+            array[i] = array[i + 1]
+        }
+        return res
+    }
+
+    override fun set(index: Int, element: T): T {
+        boundsCheck(index)
+        return when (size) {
+            1 -> single.also { elements = element }
+            in 2..MAX_ARRAY_SIZE -> array[index].also { array[index] = element }
+            else -> list.set(index, element)
+        }
+    }
+
+    companion object {
+        const val MAX_ARRAY_SIZE = 5
+    }
+
 }
--- test/src/test/kotlin/me/arkadybazhanov/au/java/hw2/SmartListTest.kt
+++ test/src/test/kotlin/me/arkadybazhanov/au/java/hw2/SmartListTest.kt
@@ -2,17 +2,31 @@ package me.arkadybazhanov.au.java.hw2
 
 
 import org.junit.jupiter.api.Test
-
-import java.util.Arrays
-import java.util.Collections
-
 import org.junit.jupiter.api.Assertions.*
+import java.lang.IndexOutOfBoundsException
 
 class SmartListTest {
 
-    private val listClass: Class<*>
-        @Throws(ClassNotFoundException::class)
-        get() = Class.forName("ru.spbau.mit.SmartList")
+    @Test
+    fun testOutOfBounds() {
+        assertThrows(IndexOutOfBoundsException::class.java) {
+            val list = newList<Int>()
+            list += 1
+            list[1]
+        }
+
+        assertThrows(IndexOutOfBoundsException::class.java) {
+            val list = newList<Int>()
+            list += 1
+            list[1] = 2
+        }
+
+        assertThrows(IndexOutOfBoundsException::class.java) {
+            val list = newList<Int>()
+            list += 1
+            list.add(2, 3)
+        }
+    }
 
     @Test
     fun testSimple() {
@@ -20,50 +34,49 @@ class SmartListTest {
 
         assertEquals(emptyList<Int>(), list)
 
-        list.add(1)
+        list += 1
         assertEquals(listOf(1), list)
 
-        list.add(2)
-        assertEquals(Arrays.asList(1, 2), list)
+        list += 2
+        assertEquals(listOf(1, 2), list)
     }
 
     @Test
     fun testGetSet() {
         val list = newList<Any>()
 
-        list.add(1)
+        list += 1
 
         assertEquals(1, list[0])
         assertEquals(1, list.set(0, 2))
         assertEquals(2, list[0])
         assertEquals(2, list.set(0, 1))
 
-        list.add(2)
+        list += 2
 
         assertEquals(1, list[0])
         assertEquals(2, list[1])
 
         assertEquals(1, list.set(0, 2))
 
-        assertEquals(Arrays.asList(2, 2), list)
+        assertEquals(listOf(2, 2), list)
     }
 
     @Test
-    @Throws(Exception::class)
     fun testRemove() {
         val list = newList<Any>()
 
-        list.add(1)
+        list += 1
         list.removeAt(0)
         assertEquals(emptyList<Any>(), list)
 
-        list.add(2)
+        list += 2
         list.remove(2 as Any)
         assertEquals(emptyList<Any>(), list)
 
-        list.add(1)
-        list.add(2)
-        assertEquals(Arrays.asList(1, 2), list)
+        list += 1
+        list += 2
+        assertEquals(listOf(1, 2), list)
 
         list.removeAt(0)
         assertEquals(listOf(2), list)
@@ -73,12 +86,11 @@ class SmartListTest {
     }
 
     @Test
-    @Throws(Exception::class)
     fun testIteratorRemove() {
         val list = newList<Any>()
         assertFalse(list.iterator().hasNext())
 
-        list.add(1)
+        list += 1
 
         var iterator: MutableIterator<Any> = list.iterator()
         assertTrue(iterator.hasNext())
@@ -88,7 +100,7 @@ class SmartListTest {
         assertFalse(iterator.hasNext())
         assertEquals(emptyList<Any>(), list)
 
-        list.addAll(Arrays.asList(1, 2))
+        list.addAll(listOf(1, 2))
 
         iterator = list.iterator()
         assertTrue(iterator.hasNext())
@@ -106,27 +118,20 @@ class SmartListTest {
 
 
     @Test
-    @Throws(Exception::class)
     fun testCollectionConstructor() {
-        assertEquals(emptyList<T>(), newList(emptyList<Any>()))
-        assertEquals(
-                listOf<T>(1),
-                newList(listOf(1)))
-
-        assertEquals(
-                Arrays.asList<T>(1, 2),
-                newList(Arrays.asList(1, 2)))
+        assertEquals(listOf<Int>(), newList(listOf<Int>()))
+        assertEquals(listOf(1), newList(listOf(1)))
+        assertEquals(listOf(1, 2), newList(listOf(1, 2)))
     }
 
     @Test
-    @Throws(Exception::class)
     fun testAddManyElementsThenRemove() {
         val list = newList<Any>()
         for (i in 0..6) {
-            list.add(i + 1)
+            list += i + 1
         }
 
-        assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7), list)
+        assertEquals(listOf(1, 2, 3, 4, 5, 6, 7), list)
 
         for (i in 0..6) {
             list.removeAt(list.size - 1)
@@ -136,37 +141,14 @@ class SmartListTest {
         assertEquals(emptyList<Any>(), list)
     }
 
-    private fun <T> newList(): MutableList<T> {
-        try {
-            return listClass.getConstructor().newInstance() as List<T>
-        } catch (e: InstantiationException) {
-            throw RuntimeException(e)
-        } catch (e: ClassNotFoundException) {
-            throw RuntimeException(e)
-        } catch (e: IllegalAccessException) {
-            throw RuntimeException(e)
-        } catch (e: NoSuchMethodException) {
-            throw RuntimeException(e)
-        } catch (e: InvocationTargetException) {
-            throw RuntimeException(e)
-        }
 
-    }
+    private val listClass: Class<*>
+        get() = Class.forName("me.arkadybazhanov.au.java.hw2.SmartList")
 
-    private fun <T> newList(collection: Collection<T>): List<T> {
-        try {
-            return listClass.getConstructor(Collection::class.java).newInstance(collection) as List<T>
-        } catch (e: InstantiationException) {
-            throw RuntimeException(e)
-        } catch (e: ClassNotFoundException) {
-            throw RuntimeException(e)
-        } catch (e: IllegalAccessException) {
-            throw RuntimeException(e)
-        } catch (e: NoSuchMethodException) {
-            throw RuntimeException(e)
-        } catch (e: InvocationTargetException) {
-            throw RuntimeException(e)
-        }
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> newList(): MutableList<T> = listClass.getConstructor().newInstance() as MutableList<T>
 
-    }
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> newList(collection: Collection<T>): MutableList<T> =
+        listClass.getConstructor(Collection::class.java).newInstance(collection) as MutableList<T>
 }
