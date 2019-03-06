39d3e88da88eaf2e3247f1e6e49a73d7fc603a19
Arkady Bazhanov <arkady.bazhanov@gmail.com>
2019-03-06 16:05:12 +0200

    Add test2

---
+++ test2/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.3.20"
+}
+
+group = "me.arkadybazhanov.au"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    testCompile(kotlin("test-junit5"))
+    testCompile(kotlin("reflect"))
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
