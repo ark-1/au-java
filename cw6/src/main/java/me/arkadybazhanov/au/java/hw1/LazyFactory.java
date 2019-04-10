0400539e0f5d85f8b430e8c821bc97841be086c5
Arkady Bazhanov <arkady.bazhanov@gmail.com>
2019-04-10 16:14:24 +0300

    Add lock-free

--- cw6/src/main/java/me/arkadybazhanov/au/java/hw1/LazyFactory.java
+++ cw6/src/main/java/me/arkadybazhanov/au/java/hw1/LazyFactory.java
@@ -1,6 +1,9 @@
 package me.arkadybazhanov.au.java.hw1;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
 public class LazyFactory {
@@ -40,22 +43,22 @@ public class LazyFactory {
         };
     }
 
-    public static <T> Lazy<T> createLazy3(Supplier<? extends T> supplier) {
-        return new Lazy<T>() {
-            private T value = null; // TODO
-//            private volatile boolean calculated = new AtomicBoolean(false);
+//    private static class Box<T> {
+//        public T value;
+//
+//        public Box(T value) {
+//            this.value = value;
+//        }
+//    }
+
+    public static <T> Lazy<T> createLazyLockFree(Supplier<? extends T> supplier) {
+        return new Lazy<>() {
+            private volatile AtomicReference<Optional<T>> value = new AtomicReference<>(null);
 
             @Override
             public T get() {
-//                if (!calculated) {
-//                    synchronized (this) {
-//                        if (!calculated) {
-//                            value = supplier.get();
-//                            calculated = true;
-//                        }
-//                    }
-//                }
-                return value;
+                //noinspection OptionalGetWithoutIsPresent
+                return value.updateAndGet(x -> Objects.requireNonNullElseGet(x, () -> Optional.of(supplier.get()))).get();
             }
         };
     }
